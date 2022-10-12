aee925868ea0b8641685638acf23120cd2ffc8c1
Mehmet Ali Celik <malicelk71@gmail.com>
2022-10-12 08:29:57 +0300

    Repeats projects have been created

---
+++ src/Day07/MainApp.java
@@ -0,0 +1,13 @@
+package Day07;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Yeni tekrara başlandı");
+        System.out.println(8-1);
+        cıkarma();
+    }
+    public static void cıkarma(){
+        System.out.println(9-4);
+
+    }
+}
---
+++ src/Day07/Variables.java
@@ -0,0 +1,19 @@
+package Day07;
+
+public class Variables {
+    public static void main(String[] args) {
+        /*= operatörü (assigment), sağında bulunan sabit bir değer (Literal) veya bir işlemin sonucunu
+        solundaki değişkenin (Variable) temsil etttiği hafıza bölgesine kaydeder.*/
+        int age=28;
+
+
+        //Dikdörtgenin alanını ve çevresini hesaplayınız.
+        int kısakenar=7;
+        int uzunkenar=13;
+
+        System.out.println("Dikdörtgenin Alanı = " + kısakenar*uzunkenar);
+        System.out.println("Dikdörtgenin Çevresi = " +( (2*kısakenar) + (2*uzunkenar)));
+
+
+    }
+}
